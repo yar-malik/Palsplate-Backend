dbe2ae2b7d02706e2605af8bf3bf2d071171e7a9
Asfandyar <malikasfandyarashraf@gmail.com>
2017-05-15 16:11:02 +0200

    changing procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java -jar target/palsplate-backend-1.0-SNAPSHOT.jar
+web: java $JAVA_OPTS -cp target/classes/:target/dependency/* com.jersey.Application
--- src/main/java/com/jersey/config/SqlInitialization.java
+++ src/main/java/com/jersey/config/SqlInitialization.java
@@ -11,9 +11,7 @@ import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.support.TransactionTemplate;
-
 import javax.sql.DataSource;
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Properties;
 import org.apache.log4j.LogManager;

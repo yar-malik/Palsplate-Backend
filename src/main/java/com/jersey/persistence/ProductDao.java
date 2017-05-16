edb8ef53d6c438d84ee6de35979bd18a86910c44
Asfandyar <malikasfandyarashraf@gmail.com>
2017-05-16 03:48:07 +0200

    Removed unnecessary and redundant files

--- src/main/java/com/jersey/persistence/ProductDao.java
+++
@@ -1,8 +0,0 @@
-package com.jersey.persistence;
-
-import com.jersey.representations.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface ProductDao extends JpaRepository<Product, Long> {
-}

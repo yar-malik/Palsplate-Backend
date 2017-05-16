3543e1dc571a76c7816fdc0a37375e9af6b0e058
Asfandyar <malikasfandyarashraf@gmail.com>
2017-05-16 02:20:59 +0200

    Queries added

--- README.md
+++ README.md
@@ -16,9 +16,30 @@ Populate palsplate-demo database with some dummy data.
 
 Use the following queries: 
 
-`INSERT INTO member VALUES(1, 'Asfandyar', 'Malik', 'malikasfandyarashraf@gmail.com')`
+```
+INSERT INTO member VALUES(1, 'Asfandyar', 'Malik', 'malikasfandyarashraf@gmail.com')
 
-`INSERT INTO product VALUES (1, 'Cheese', 'euro', 14, 12, 1);`
+INSERT INTO product VALUES (1, 'Cheese', 'euro', 14, 12, 1);`
+INSERT INTO product VALUES (3, 'Yogurt', 'euro', 2, 1, 1);
+
+INSERT INTO login VALUES (1, 'Asfandyar', 'mypass');
+INSERT INTO login VALUES (2, 'Giroud', 'giroud_pass');
+INSERT INTO login VALUES (3, 'ramsey', 'ramsey_mypass');
+
+INSERT INTO PERSON VALUES (1, 'asfandyar@gmail.com', 'Asfandyar', 'Malik', '01575117434', 'islamabad', 'best chef ever', true, 1)	
+INSERT INTO PERSON VALUES (2, 'giroud@gmail.com', 'Oliver', 'Giroud', '+1575117434', 'paris', 'love food ever', true, 2)	
+INSERT INTO PERSON VALUES (3, 'ramsey@gmail.com', 'Aaron', 'Ramsey', '+157511743434', 'wales', 'foodie', true, 3)	
+
+INSERT INTO Customer VALUES (1, 1)
+
+INSERT INTO Cook VALUES (1, 2)
+INSERT INTO Cook VALUES (2, 3)
+
+INSERT INTO FOOD VALUES(2, 'Omlette', '2011-05-16 15:36:38', '2011-06-16 15:36:38', 'French delight', 5, 3, 'vegetarian', 'French', 0.45, 0.56, true, 1)
+INSERT INTO FOOD VALUES(3, 'burger', '2011-05-16 15:36:38', '2011-06-16 15:36:38', 'fast food delight', 5, 3, 'meat', 'fast_food', 0.45, 0.56, true, 2)
+```
+
+
 
 
 - - - -
--- src/main/resources/query.sql
+++ src/main/resources/query.sql
@@ -26,10 +26,7 @@ CREATE TABLE login
   id serial NOT NULL,
   username character varying NOT NULL,
   password character varying NOT NULL,
-  CONSTRAINT pk_login_id PRIMARY KEY (id),
-  CONSTRAINT fk_login__personFOREIGN KEY (person_id)
-      REFERENCES person (id) MATCH SIMPLE
-      ON UPDATE CASCADE ON DELETE CASCADE
+  CONSTRAINT pk_login_id PRIMARY KEY (id)
 )
 
 CREATE TABLE person
@@ -38,17 +35,22 @@ CREATE TABLE person
   email character varying NOT NULL,
   first_name character varying NOT NULL,
   last_name character varying NOT NULL,
-  phone_number character NOT NULL,
+  phone_number character varying NOT NULL,
   address character varying NOT NULL,
   description character varying NOT NULL,
   is_photo_public boolean NOT NULL,
-  CONSTRAINT pk_person_id PRIMARY KEY (id)
+  login_id bigint NOT NULL,
+  CONSTRAINT pk_person_id PRIMARY KEY (id),
+  CONSTRAINT fk_person__login FOREIGN KEY (login_id)
+    REFERENCES login (id) MATCH SIMPLE
+    ON UPDATE CASCADE ON DELETE CASCADE
 )
 
 CREATE TABLE customer
 (
   id serial NOT NULL,
   CONSTRAINT pk_customer_id PRIMARY KEY (id),
+  person_id bigint NOT NULL,
   CONSTRAINT fk_customer__person FOREIGN KEY (person_id)
       REFERENCES person (id) MATCH SIMPLE
       ON UPDATE CASCADE ON DELETE CASCADE
@@ -57,6 +59,7 @@ CREATE TABLE customer
 CREATE TABLE cook
 (
   id serial NOT NULL,
+  person_id bigint NOT NULL,
   CONSTRAINT pk_cook_id PRIMARY KEY (id),
   CONSTRAINT fk_cook__person FOREIGN KEY (person_id)
     REFERENCES person (id) MATCH SIMPLE
@@ -77,6 +80,7 @@ CREATE TABLE food
   lat numeric NOT NULL,
   lon numeric NOT NULL,
   is_active boolean NOT NULL,
+  cook_id bigint NOT NULL,
   CONSTRAINT pk__id PRIMARY KEY (id),
   CONSTRAINT fk_food__cook FOREIGN KEY (cook_id)
     REFERENCES cook (id) MATCH SIMPLE

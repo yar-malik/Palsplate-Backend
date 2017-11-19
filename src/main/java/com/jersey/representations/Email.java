6606e25d6c41f4554152f3e4f21e8cd2a8dfd9d8
Asfandyar <malikasfandyarashraf@gmail.com>
2017-11-20 01:01:20 +0500

    Email new templates added. Email addition of locale parameter made

--- src/main/java/com/jersey/representations/Email.java
+++ src/main/java/com/jersey/representations/Email.java
@@ -3,9 +3,9 @@ package com.jersey.representations;
 
 public class Email {
 
+    public String type;
     public String subject;
     public String recipient;
-    public String type;
     public String name;
-
+    public String locale;
 }

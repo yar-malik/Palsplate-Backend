2aadcbf3ac34674daebb02475c3b1d65d3d88a68
Muhammad Omer Saeed <muhammad.omar555@gmail.com>
2017-12-04 14:39:59 +0100

    fixed bug, added facebook pic upload to cloudinary

--- src/main/java/com/jersey/Authorization/Config/Facebook/FacebookConnectionSignup.java
+++ src/main/java/com/jersey/Authorization/Config/Facebook/FacebookConnectionSignup.java
@@ -1,9 +1,13 @@
 package com.jersey.Authorization.Config.Facebook;
 
-import com.jersey.persistence.LocationPersonDao;
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
 import com.jersey.persistence.PersonDao;
-import com.jersey.representations.LocationPerson;
 import com.jersey.representations.Person;
+import com.jersey.resources.PersonResource;
+import org.apache.commons.io.FileUtils;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.social.connect.Connection;
 import org.springframework.social.connect.ConnectionSignUp;
@@ -11,16 +15,29 @@ import org.springframework.social.facebook.api.Facebook;
 import org.springframework.social.facebook.api.User;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Date;
+import java.util.Map;
+
 import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
 
 @Service
 public class FacebookConnectionSignup implements ConnectionSignUp{
 
+    private static final Logger log = LogManager.getLogger(PersonResource.class);
+
     @Autowired
     private PersonDao personRepository;
 
     @Autowired
-    private LocationPersonDao locationPersonDao;
+    private Cloudinary cloudinary;
+
+    private final int IMAGE_WIDTH = 640;
+
+    private final int IMAGE_HEIGHT = 640;
+
 
     @Override
     public String execute(Connection<?> connection) {
@@ -32,6 +49,8 @@ public class FacebookConnectionSignup implements ConnectionSignUp{
 
         Person person = personRepository.findByEmail(userProfile.getEmail());
 
+
+
         if(person == null){
             //create a new user in our local db if it already doesn't exist
             person = new Person();
@@ -40,31 +59,49 @@ public class FacebookConnectionSignup implements ConnectionSignUp{
             person.setEmail(userProfile.getEmail());
             person.setRoles("ROLE_USER_FACEBOOK");
             person.setPassword(randomAlphabetic(8));
-
-            LocationPerson locationPerson = locationPersonDao.findByPerson_id(person.getId());
-
-            if(userProfile.getLocation() != null){
-                locationPerson.setAddress(userProfile.getLocation().getName());
-            }
-            else{
-                //temp since we have not null constraint
-                locationPerson.setAddress("This is a placeholder for address");
-            }
+            UploadFacebookProfilePicToCloudinary(connection, person);
 
             if(userProfile.getAbout() != null){
                 person.setDescription(userProfile.getAbout());
             }
             else{
-                //temp because we have not null constraint
-                person.setDescription("This is a placeholder for description");
+                person.setDescription("");
             }
 
-            //temp placeholder
-            person.setPhoneNumber("123456789");
+            person.setPhoneNumber("");
 
             personRepository.save(person);
         }
 
         return person.getEmail();
+    }
+
+    private void UploadFacebookProfilePicToCloudinary(Connection<?> connection, Person person){
+
+        String profileImageURLLarge = new StringBuilder(connection.getImageUrl())
+                .append("?").append("width=").append(IMAGE_WIDTH)
+                .append("&").append("height=").append(IMAGE_HEIGHT)
+                .toString();
+
+        File tempFile;
+        Map uploadResult = null;
+
+        try {
+            tempFile = File.createTempFile("profilepic", ".png");
+            tempFile.deleteOnExit();
+            FileUtils.copyURLToFile(new URL(profileImageURLLarge), tempFile);
+            uploadResult = cloudinary.uploader().upload(tempFile, ObjectUtils.emptyMap());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        log.info("cloudinary secure_url: " + uploadResult.get("secure_url"));
+        log.info("cloudinary public_id: " + uploadResult.get("public_id"));
+        log.info("cloudinary original_filename: " + uploadResult.get("original_filename"));
+
+        person.setPhotoName(new StringBuilder((String)uploadResult.get("original_filename")).append(new Date().toString()).append(".png").toString());
+        person.setPhotoPublicId(uploadResult.get("public_id").toString());
     }
 }
